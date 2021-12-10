d5e7c0e9343b91af9fb89bedcf9cfb182b3a4a21
takau-jp <takausoccer@icloud.com>
2021-12-10 12:32:41 +0900

    ft_strjoin

--- srcs/string/ft_strjoin.c
+++ srcs/string/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: stanaka < stanaka@student.42tokyo.jp>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 20:13:29 by macbook_air       #+#    #+#             */
-/*   Updated: 2021/12/04 23:21:06 by stanaka          ###   ########.fr       */
+/*   Updated: 2021/12/10 11:58:37 by stanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*tmp;
+	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
 
 	if (!s1 || !s2)
 		return (NULL);
-	tmp = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!tmp)
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!str)
 		return (NULL);
-	*tmp = '\0';
-	ft_strcat(tmp, s1);
-	ft_strcat(tmp, s2);
-	return (tmp);
+	ft_memcpy(str, s1, s1_len);
+	ft_memcpy(str + s1_len, s2, s2_len);
+	str[s1_len + s2_len] = '\0';
+	return (str);
 }
